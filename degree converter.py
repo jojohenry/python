bda0fee692e70117fe94d05f340a3111ef8cb16e
jojohenry <jshenry91@yahoo.com>
2023-01-02 15:12:01 +0000

    added degree converter.py

---
+++ degree converter.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("What is the temperature (in Fahrenheit) you would like to covert to Celsius? "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is", round(celsius, 2), "C")
+
