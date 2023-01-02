43b2302e35b837d9d6569e0984b47dff3da8cd7a
jojohenry <jshenry91@yahoo.com>
2023-01-02 15:23:58 +0000

    added list.py

---
+++ list.py
@@ -0,0 +1,18 @@
+users = []
+
+users.append("melissa")
+users.append("susan")
+users.append("kelly")
+
+#this code prints the first three names 
+print(users)
+
+#this code prints the list in reverse order
+print (list(reversed(users)))
+
+#this code adds the four names below to the list
+users += ("marsha", "abigail", "amanda", "jodi")
+
+
+#this code prints the names in the fifth, sixth and seventh place
+print(users[4:7])
