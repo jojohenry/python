856bf067ba746e1234370b0a69b8d2ca488fc64a
jojohenry <jshenry91@yahoo.coom>
2023-01-03 03:03:23 +0000

    added mutations.py

--- mutations.py
+++ mutations.py
@@ -1,5 +1,5 @@
+#Changes a character at a given index
 def mutate_string(string, position, character):
-    '''Changes a character at a given index'''
     return string[:position] + character + string[position + 1:]
 
 if __name__ == '__main__':
