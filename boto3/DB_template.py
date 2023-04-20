e8363244e89899725880854b86dc89ba7d89bbc3
Jodiann Henry <109822667+jojohenry@users.noreply.github.com>
2023-04-20 17:03:30 -0400

    Create DB_template.py

---
+++ boto3/DB_template.py
@@ -0,0 +1,72 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.create_table(
+    TableName='Testing_here',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
+print("Table status:", table.table_status)
+
+
+'''
+import boto3
+import json
+import decimal
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+table = dynamodb.Table('Movies')
+
+title = "The Big New Movie"
+year = 2015
+
+response = table.put_item(
+   Item={
+        'year': year,
+        'title': title,
+        'info': {
+            'plot':"Nothing happens at all.",
+            'rating': decimal.Decimal(0)
+        }
+    }
+)
+
+print("PutItem succeeded:")
+print(json.dumps(response, indent=4, cls=DecimalEncoder))
+'''
