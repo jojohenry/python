7e8338196f43c40abb9703a34f9aee277a417fed
Jodiann Henry <109822667+jojohenry@users.noreply.github.com>
2023-04-20 17:21:03 -0400

    Create json_input.py

---
+++ boto3/json_input.py
@@ -0,0 +1,38 @@
+import boto3
+import argparse
+import json
+
+# This uses a json string as an input 
+parser = argparse.ArgumentParser(description="Provides translation between one source language and another of the same set of languages.")
+parser.add_argument(
+    '--file',
+    dest='filename',
+    help="The path to the input file. The file should be valid json",
+    required=True)
+    
+
+args = parser.parse_args()
+
+
+#Functions
+def open_input():
+    with open(args.filename) as file_object:
+        contents = json.load(file_object)
+        return contents['Input'][0]
+
+def translate_text(**kwargs):
+    client = boto3.client('translate')
+    response = client.translate_text(**kwargs)
+    print(response)
+
+
+
+def main():
+    kwargs = open_input()
+    translate_text(**kwargs)
+
+        
+        
+
+if __name__=="__main__":
+    main()
