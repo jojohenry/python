14388cc9b7ac45b691efbf16a3440f2d787b5040
Jodiann Henry <109822667+jojohenry@users.noreply.github.com>
2023-05-15 16:16:41 -0400

    Create Day26-web.py

---
+++ 30DaysofPython/Day26 - PythonForWeb/Day26-web.py
@@ -0,0 +1,7 @@
+# FLASK
+# You can find the application I built using Flask in the repository 'Readers-Cabin'
+# link : https://github.com/jojohenry/Readers-Cabin
+
+# DJANGO
+# You can find the application I built using Django in the repository 'CS50w-Commerce'
+# link : https://github.com/jojohenry/CS50w-Commerce
